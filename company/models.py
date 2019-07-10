5f09e971568c49b9e455d17a15fd81cedf349aa2
Ubuntu <ubuntu@ip-172-31-41-134.ec2.internal>
2019-07-10 21:06:42 +0000

    editied company models, roughly displaying company info, can edit company info.

--- company/models.py
+++ company/models.py
@@ -1,29 +1,47 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
         
 class CompanyDetail(models.Model):    
     INDUSTRY = (
-            ('Agriculture, forestry, fishing and hunting', 'Agriculture, forestry, fishing and hunting'),
-            ('Construction', 'Construction'),
-            ('Manufacturing', 'Manufacturing'),
-            ('Transportation equipment manufacturing', 'Transportation equipment manufacturing'),
-            ('Service industries', 'Service industries'),
-            ('Wholesale Trade', 'Wholesale Trade'),
-            ('Retail Trade', 'Retail Trade'),
-            ('Transportation and warehousing', 'Transportation and warehousing'),
-            ('Information and cultural industries', 'Information and cultural industries'),
-            ('Finance and insurance', 'Finance and insurance'),
-            ('Real estate and rental and leasing', 'Real estate and rental and leasing'),
-            ('Professional, scientific, and technical services', 'Professional, scientific, and technical services'),
-            ('Management of companies and enterprises', 'Management of companies and enterprises'),
-            ('Administrative and support, waste management and remediation services', 'Administrative and support, waste management and remediation services'),
+            ('AG', 'Agriculture, forestry, fishing and hunting'),
+            ('CN', 'Construction'),
+            ('MG', 'Manufacturing'),
+            ('TR', 'Transportation equipment manufacturing'),
+            ('SI', 'Service industries'),
+            ('WT', 'Wholesale Trade'),
+            ('RT', 'Retail Trade'),
+            ('TW', 'Transportation and warehousing'),
+            ('IC', 'Information and cultural industries'),
+            ('FI', 'Finance and insurance'),
+            ('RE', 'Real estate and rental and leasing'),
+            ('PS', 'Professional, scientific, and technical services'),
+            ('MA', 'Management of companies and enterprises'),
+            ('AD', 'Administrative and support, waste management and remediation services'),
         )
         
+    business_name = models.CharField(max_length=254, null=True, default="Business Name", blank=True)
     industry = models.CharField(max_length=254, null=True, choices=INDUSTRY, default="Please pick an industry", blank=True)
     contact_email = models.EmailField(default="email@email.com", null=True, blank=True)
     about_us = models.CharField(max_length=254, null=True, default="Information about your company - where you're from, what you do!", blank=True)
     address = models.CharField(max_length=254, null=True, default="Business Address", blank=True)
-    image = models.ImageField(upload_to='images', null=True, blank=True)
+    logo = models.ImageField(upload_to='images', null=True, blank=True)
     created_by = models.ForeignKey(User, null=True, default=1, on_delete=models.SET_DEFAULT)
+    overall_rating = models.IntegerField(default=0, blank=True)
+    
+class CompanyReview(models.Model):
+    RATINGS = (
+        (1,'1'),
+        (2,'2'),
+        (3,'3'),
+        (4,'4'),
+        (5,'5'),
+        )
+    
+    created_by = models.ForeignKey(User, null=True, default=1, on_delete=models.SET_DEFAULT)
+    company_reviewed = models.CharField(max_length=254, null=True, default="Please pick an industry", blank=False)
+    content = models.CharField(max_length=254, null=True, default="Review Content", blank=False)
+    rating = models.IntegerField(choices=RATINGS, blank=False)
+    date_created = models.DateTimeField(blank=True, null=True, default=timezone.now)
